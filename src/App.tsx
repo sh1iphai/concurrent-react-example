365acd2bf7de23914c89ac2b4f4dcf5f9c7a0853
sh1iphai <cfi3198@gmail.com>
2023-08-21 20:48:30 +0900

    add suspense-multiple-base

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { Suspense } from "react";
+import { Suspend } from "./Suspend";
+
 export const App = () => {
-  return <>Hello React</>;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Suspend />
+    </Suspense>
+  );
 };
