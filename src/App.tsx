92d30d2fe79c6360337812c3fbac036c4980f016
sh1iphai <cfi3198@gmail.com>
2023-08-21 21:02:57 +0900

    suspense-multiple

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Suspense } from "react";
 import { Suspend } from "./Suspend";
+import { Message } from "./Message";
 
 export const App = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Suspend />
+      <Message />
     </Suspense>
   );
 };
